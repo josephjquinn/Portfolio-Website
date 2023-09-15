cba1803b31ad2dfb00b805d62637ecbbb0a83871
jquinn <jquinn2005@outlook.com>
2023-09-14 21:56:19 -0500

    added project videos

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ function toggleMenu()
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+function toggleVideo(videoId)
+{
+    var videoOverlay = document.getElementById(videoId);
+    var videoPlayer = videoOverlay.querySelector('.video-player');
+    if (videoOverlay.style.display === 'block') {
+        videoOverlay.style.display = 'none';
+        videoPlayer.pause(); // Pause the video
+    } else {
+        videoOverlay.style.display = 'block';
+        videoPlayer.play(); // Play the video
+    }
+}
